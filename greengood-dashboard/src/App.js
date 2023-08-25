ab836eff42c91f0ed774a2ec48ec0b19dea73f38
DiegoMoyan0 <diegomoyano02@gmail.com>
2023-08-24 22:32:14 -0300

    add files dashboard

--- greengood-dashboard/src/App.js
+++ greengood-dashboard/src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Topnav from './components/Topnav';
+import Sidenav from './components/Sidenav';
+import Cards from './components/Cards';
+import Charts from './components/Charts';
+import Datatable from './components/Datatable';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <body class="sb-nav-fixed">
+        <Topnav />
+        <div id="layoutSidenav">
+          <div id="layoutSidenav_nav">
+            <Sidenav />
+          </div>
+        </div>
+        <div id="layoutSidenav_content">
+          <main>
+            <div class="container-fluid px-4">
+              <h1 class="mt-4">Dashboard</h1>
+              <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active">Dashboard</li>
+              </ol>
+              <Cards />
+              <Charts />
+              <Datatable />
+            </div>
+          </main>
+
+          <Footer />
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+      </body>
+    </>
   );
 }
 
---
+++ greengood-dashboard/src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+
+    );
+}
+
+export default Footer;
