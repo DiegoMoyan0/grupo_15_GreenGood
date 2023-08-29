05c50b635118a4f0d8fec3f842cd6f595ed85644
JRMZdev <129340846+JRMZdev@users.noreply.github.com>
2023-08-29 02:27:05 -0500

    Fix last user image fetch on dashboard

--- greengood-dashboard/dashboard/src/components/UserComponents/LastUserCard.js
+++ greengood-dashboard/dashboard/src/components/UserComponents/LastUserCard.js
@@ -1,34 +1,58 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 function LastUserCard() {
-  const [userData, setUserData] = useState(null)
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:3001/api/user/last-registered');
-        const data = await response.json()
-        if (data.meta.success) {
-          setUserData(data);
+
+        const userResponse = await fetch('http://localhost:3001/api/user/last-registered')
+        const userData = await userResponse.json();
+
+        if (userData.meta.success) {
+
+
+          const userId = userData.user.id
+          const userImageResponse = await fetch(`http://localhost:3001/api/user/image/${userId}`)
+
+
+          const userImageData = await userImageResponse.json()
+
+          console.log(userImageData)
+
+
+
+          if (userImageData.meta.success) {
+            userData.userImage = userImageData.userImage
+            console.log(userData.userImage)
+          }
+
+          setUserData(userData)
         }
       } catch (error) {
-        console.error('Error fetching data:', error)
+        console.error('Error fetching data:', error);
       }
     };
-
-    console.log(userData)
+    
 
     fetchData()
-  }, [])
+    
+  }, []);
+
+  if (!userData) {
+    return <div>Cargando información...</div>
+  }
 
 
   const registrationDate = new Date(userData.user.created_at).toLocaleDateString()
 
   return (
+    
     <div className="card">
       <div className="card-header">Último usuario registrado</div>
       <div className="card-body">
-        <img src={userData.user_image} alt="User" className="user-image" />
+        <img src={userData.userImage} alt="User" className="user-image" />
         <h5 className="card-title">{userData.user.first_name} {userData.user.last_name}</h5>
         <p className="card-text">Tipo: {userData.user.type}</p>
         <p className="card-text">Provincia: {userData.user.address.province}</p>
