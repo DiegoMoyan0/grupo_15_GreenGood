a2c6fa51d47377b1df6a3f147c6d7a875ff81489
DiegoMoyan0 <diegomoyano02@gmail.com>
2023-08-30 22:48:55 -0300

    add card last product and user

--- greengood-dashboard/dashboard/src/components/ContentRowProducts.js
+++ greengood-dashboard/dashboard/src/components/ContentRowProducts.js
@@ -67,6 +67,29 @@ function ContentRowProducts() {
               </div>
             </div>
           </div>
+
+        );
+      })}
+      {arrayMetrics.map((box, index) => {
+        return (
+          <div className="col-md-4 mb-4" key={index + 'ContentRowMovies'}>
+            <div className={`card border-left-${box.borderColor} shadow h-100 py-2`}>
+              <div className="card-body">
+                <div className="row no-gutters align-items-center">
+                  <div className="col mr-2">
+                    <div className={`text-xs font-weight-bold text-${box.borderColor} text-uppercase mb-1`}>
+                      {box.titulo}
+                    </div>
+                    <div className="h5 mb-0 font-weight-bold text-gray-800">{box.cifra}</div>
+                  </div>
+                  <div className="col-auto">
+                    <i className={`fas fa-${box.icono} fa-2x text-gray-300`}></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
         );
       })}
     </React.Fragment>
